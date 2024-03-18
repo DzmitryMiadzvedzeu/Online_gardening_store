1c1e97bf1ba4e069968ddcae2685284a8d27fb84
stop2 <stop292005@gmail.com>
2024-03-18 07:03:24 +0100

    .

--- src/main/java/org/shop/com/service/UserService.java
+++ src/main/java/org/shop/com/service/UserService.java
@@ -1,9 +1,10 @@
 package org.shop.com.service;
 
 import org.shop.com.entity.UserEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface UserService {
 
     void deleteUserById(Long id);

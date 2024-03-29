987950d48009df8a5bcc77c95b9f13d3e24df400
mac <andriyets2004@gmail.com>
2024-03-29 14:12:05 +0100

    mary

--- src/main/java/org/shop/com/mapper/ProductMapper.java
+++ src/main/java/org/shop/com/mapper/ProductMapper.java
@@ -1,5 +1,6 @@
 package org.shop.com.mapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.shop.com.dto.ProductCreateDto;
 import org.shop.com.dto.ProductDto;
@@ -10,10 +11,25 @@ public interface ProductMapper {
 
    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
 
+   @Mapping(source = "name", target = "name")
+   @Mapping(source = "description", target = "description")
+   @Mapping(source = "price", target = "price")
+   @Mapping(source = "image", target = "image")
+//   @Mapping(source = "category", target = "categoryId")
    ProductDto toDto(ProductEntity productEntity);
 
+   @Mapping(source = "name", target = "name")
+   @Mapping(source = "description", target = "description")
+   @Mapping(source = "price", target = "price")
+   @Mapping(source = "image", target = "image")
+//   @Mapping(source = "categoryId", target = "category")
    ProductEntity createDtoToEntity(ProductCreateDto productCreateDto);
 
+   @Mapping(source = "name", target = "name")
+   @Mapping(source = "description", target = "description")
+   @Mapping(source = "price", target = "price")
+   @Mapping(source = "image", target = "image")
+//   @Mapping(source = "categoryId", target = "category")
    ProductEntity toEntity(ProductDto dto);
 
 }
